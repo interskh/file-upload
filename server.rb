8e6b9e609d7693b4434849dcaffd56fb80683a3f
Kyle Sun <interskh@gmail.com>
2013-10-28 18:35:51 -0700

    / show the list of files

--- server.rb
+++ server.rb
@@ -2,19 +2,30 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-# Handle GET-request (Show the upload form)
-get "/upload" do
-  haml :upload
-end
 
-# Handle POST-request (Receive and save the uploaded file)
-post "/upload" do 
-  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
-    f.write(params['myfile'][:tempfile].read)
+class App < Sinatra::Application
+
+  # Handle GET-request (Show the upload form)
+  get "/upload" do
+    haml :upload
   end
-  return "The file was successfully uploaded!"
-end
 
-get "/uploads/:file" do
-  send_file(File.join('uploads', params[:file]), :disposition => 'inline')
+  # Handle POST-request (Receive and save the uploaded file)
+  post "/upload" do
+    File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+      f.write(params['myfile'][:tempfile].read)
+    end
+    return "The file was successfully uploaded!"
+  end
+
+  get "/uploads/:file" do
+    send_file(File.join('uploads', params[:file]), :disposition => 'inline')
+  end
+
+  get "/" do
+    Dir.entries('uploads/').select{|e| e !~ /^\./}.map do |e|
+      "<p><a href='/uploads/#{e}'>#{e}</a></p>"
+    end
+  end
+
 end
